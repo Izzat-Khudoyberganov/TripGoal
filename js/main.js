928bb72cbdcf54d5a10fc999da022477cdc90b4e
izzat <izzatabs@gmail.com>
2023-10-25 13:01:31 +0500

    Added banner section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 let burgerMenu = document.querySelector(".burger-menu");
 let mobileMenu = document.querySelector(".header-actions");
+let bannerItem = document.querySelectorAll(".banner-item");
+
+console.log(bannerItem);
 
 burgerMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
     burgerMenu.classList.toggle("show");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    bannerItem.forEach((el) => {
+        el.style = `transform: translateX(0);
+                    opacity: 1`;
+    });
 });
