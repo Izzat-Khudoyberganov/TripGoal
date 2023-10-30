448fed8ea104ccabad4983d4199b3c6c053f1ff8
izzat <izzatabs@gmail.com>
2023-10-30 13:04:29 +0500

    Added travel section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 let burgerMenu = document.querySelector(".burger-menu");
 let mobileMenu = document.querySelector(".header-actions");
 let bannerItem = document.querySelectorAll(".banner-item");
+let travelContentItems = document.querySelectorAll(".travel-content_items");
 
-console.log(bannerItem);
+travelContentItems.forEach((el) => {
+    el.addEventListener("mousemove", rotate);
+    el.addEventListener("mouseout", defaultRotate);
+});
+
+function rotate(e) {
+    let halfHeight = this.offsetHeight / 2;
+    let x = e.offsetX - halfHeight;
+    let y = e.offsetY - halfHeight;
+    this.style.transform = `rotateX(${-x / 4}deg) rotateY(${-y / 4}deg)`;
+}
+
+function defaultRotate() {
+    this.style.transform = `rotate(0deg)`;
+}
 
 burgerMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
