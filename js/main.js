b193f80bd4f387344532aaecb1cf0ea2f7a2b79c
izzat <izzatabs@gmail.com>
2023-11-01 13:12:53 +0500

    added swiper

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 let burgerMenu = document.querySelector(".burger-menu");
 let mobileMenu = document.querySelector(".header-actions");
 let bannerItem = document.querySelectorAll(".banner-item");
-let travelContentItems = document.querySelectorAll(".travel-content_items");
-
-travelContentItems.forEach((el) => {
-    el.addEventListener("mousemove", rotate);
-    el.addEventListener("mouseout", defaultRotate);
-});
-
-function rotate(e) {
-    let halfHeight = this.offsetHeight / 2;
-    let x = e.offsetX - halfHeight;
-    let y = e.offsetY - halfHeight;
-    this.style.transform = `rotateX(${-x / 4}deg) rotateY(${-y / 4}deg)`;
-}
-
-function defaultRotate() {
-    this.style.transform = `rotate(0deg)`;
-}
 
 burgerMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
@@ -29,4 +12,13 @@ window.addEventListener("DOMContentLoaded", () => {
         el.style = `transform: translateX(0);
                     opacity: 1`;
     });
+});
+
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 47,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
